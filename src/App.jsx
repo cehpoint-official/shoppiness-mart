83299597bf084e0cf479eccb276febf84b76d635
uma <uma@gmail.com>
2024-04-26 00:00:32 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Cashback from './pages/Cashback'
+import Shop from './pages/Shop'
+import Work from './pages/Work'
 
 const App = () => {
   return <>
@@ -11,10 +14,14 @@ const App = () => {
       <Shoppinessmart />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/work' element={<Work/>} />
+        <Route path='/cashback' element={<Cashback />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
+    
 
 
 
---
+++ src/pages/Work.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import work1 from "../assets/work1.png"
+const Work = () => {
+  return <>
+   <div className='my-28  '>
+            <div className="grid grid-cols-12 mx-4 md:mx-16 px-4 md:px-10">
+                <div className="col-span-12 md:col-span-6">
+                    <p className="text-2xl md:text-4xl font-semibold mt-16 md:mt-12">How Works SHOPPINESSMART</p>
+                    <p className="text-base md:text-xl text-gray-500 mt-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,commodi tempora mollitia voluptatem recusandae impedit totam aperiam nesciunt doloremque magni neque placeat, laborum nisi eum quae voluptatum </p>
+                    <p className="text-base md:text-xl text-gray-500 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,commodi tempora mollitia voluptatem recusandae impedit totam aperiam nesciunt doloremque magni neque placeat, laborum nisi eum quae voluptatum </p>
+<button type="button" class="bg-[#049D8E] rounded-md py-2 px-4 text-white  mt-5">Sign up for free</button>
+
+                </div>
+                <div className="col-span-12 md:col-span-6">
+                    <div className="flex flex-wrap justify-center md:justify-start">
+                     <img src={work1 } alt="" />
+                        
+                    </div>
+
+                    
+                </div>
+            </div>
+  </div>
+  </>
+}
+
+export default Work
