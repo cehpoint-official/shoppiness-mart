45a5894cfef61650f348e1de43db03898d048932
fahim <basitabdullah1122@gmail.com>
2024-05-22 12:03:22 +0530

    Created A Routing Outlet And Form Updates

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,82 +1,154 @@
 import { useState } from "react";
 import "./Form.scss";
 import { FaCircleCheck } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import logo from "../../assets/RegisterBusiness/logo.png";
 
 const Form = () => {
-  const [pageFirst, setPageFirst] = useState(true);
-  const [pageSecond, setPageSecond] = useState(false);
-  const [pageThird, setPageThird] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const handleNextPage = () => setCurrentPage((prev) => prev + 1);
+  const handleBackPage = () => setCurrentPage((prev) => prev - 1);
 
-  const handleNextFirstPage = () => {
-    setPageSecond(true);
-    setPageFirst(false);
-  };
-  const handleNextSecondPage = () => {
-    setPageThird(true);
-    setPageSecond(false);
-  };
+  const pages = [
+    {
+      title: "Business Details",
+      content: (
+        <form className="formFirst">
+          <div className="left">
+            <div className="item">
+              <label>Business/Services Name</label>
+              <input type="text" />
+            </div>
+            <div className="item">
+              <label>Business/Services Name</label>
+              <input type="text" />
+            </div>
+            <h3>Business/Services Owner Name</h3>
+            <div className="options">
+              <label htmlFor="online">Online</label>
+              <input id="online" name="options" type="radio" />
+              <label htmlFor="offline">Offline</label>
+              <input id="offline" type="radio" name="options" />
+            </div>
+          </div>
+          <for className="right">
+            <div className="item">
+              <label>Contact number</label>
+              <p>(The number of the business man or service provider office)</p>
+              <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" />
+            </div>
 
-  const handleBackSecond = () => {
-    setPageFirst(true);
-    setPageSecond(false);
-  };
-  const handleBackThird = () => {
-    setPageThird(false);
-    setPageSecond(true);
-  };
+            <div className="item">
+              <label>Email Id</label>
+              <p>(The number of the business man or service provider office)</p>
+              <input type="email" />
+            </div>
+
+            <div className="btns">
+              <Link className="cancel" to="/register-business">
+                Cancel
+              </Link>
+              <button onClick={handleNextPage} className="save">
+                Save & Next
+              </button>
+            </div>
+          </for>
+        </form>
+      ),
+    },
+    {
+      title: "Shop Details",
+      content: (
+        <div className="formSecond">
+          <div className="left">
+            <div className="item">
+              <label>Select Catagory</label>
+              <select>
+                <option value="one">One</option>
+                <option value="one">Two</option>
+                <option value="one">Three</option>
+              </select>
+            </div>
+            <div className="item">
+              <label>Location</label>
+              <input type="text" />
+            </div>
+            <div className="item">
+              <label>PIN Code</label>
+              <input type="text" />
+            </div>
+
+            <div className="item">
+              <label>Short Description</label>
+              <p>(Write a description about your business or service)</p>
+              <textarea></textarea>
+            </div>
+          </div>
+          <div className="right">
+            <div className="item">
+              <h3>Upload Logo</h3>
+              <label htmlFor="file">
+                <p>Drag And Drop</p>
+                or
+                <p>Choose File</p>
+              </label>
+              <input type="file" id="file" />
+            </div>
+
+            <div className="item">
+              <h3>Add Banner</h3>
+              <label htmlFor="file">
+                <p>Drag And Drop</p>
+                or
+                <p>Choose File</p>
+              </label>
+              <input type="file" id="file" />
+            </div>
+
+            <div className="btns">
+              <button onClick={handleNextPage}>Next</button>
+              <button onClick={handleBackPage}>Back</button>
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Create your account",
+      content: (
+        <div className="first">
+          <h2>hey page three</h2>
+          <button onClick={handleBackPage}>Back</button>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <div className="form">
-      <h1>Register a business/Services</h1>
+      <div className="nav">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <h1>Register a business/Services</h1>
+      </div>
       <div className="stepper">
-        <div className="btn">
-          <div className={pageFirst ? "check active" : "check"}>
-            <FaCircleCheck />
+        {pages.map((page, index) => (
+          <div className="btn" key={index}>
+            <div
+              className={currentPage === index + 1 ? "check active" : "check"}
+            >
+              <FaCircleCheck />
+            </div>
+            <h3>{page.title}</h3>
           </div>
-          <button>Business Details</button>
-        </div>
-
-        <div className="btn">
-          <div className={pageSecond ? "check active" : "check"}>
-            <FaCircleCheck />
-          </div>
-          <button>Shop Details</button>
-        </div>
-
-        <div className="btn">
-          <div className={pageThird ? "check active" : "check"}>
-            <FaCircleCheck />
-          </div>
-          <button>Create your account</button>
-        </div>
+        ))}
       </div>
-
-      <div className="formContainer">
-        {pageFirst && (
-          <div className="first">
-            <h2>hey page one</h2>
-            <button onClick={handleNextFirstPage}>Next</button>
-          </div>
-        )}
-        {pageSecond && (
-          <div className="first">
-            <h2>hey page two</h2>
-            <button onClick={handleNextSecondPage}>Next</button>
-            <button onClick={handleBackSecond}>back</button>
-          </div>
-        )}
-        {pageThird && (
-          <div className="first">
-            <h2>hey page three</h2>
-            <button onClick={handleBackThird}>back</button>
-          </div>
-        )}
-      </div>
+      <div className="formContainer">{pages[currentPage - 1].content}</div>
     </div>
   );
 };
 
 export default Form;
-
-// const url =
-//   "https://inrdeals.com/fetch/stores?token=2c3f5a1662f83d1db90c9441012d4b3ffc21bbfb&id=maa443089855";
