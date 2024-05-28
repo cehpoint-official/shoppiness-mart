ea25c8265fab70f5fa3214006db6f4805b2308d6
fahim <basitabdullah1122@gmail.com>
2024-05-28 20:31:45 +0530

    api

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -8,26 +8,26 @@ const DataFetchingComponent = () => {
   
   useEffect(() => {
     const fetchData = async () => {
-      // const actual = "https://inrdeals.com/fetch/stores?token=2c3f5a1662f83d1db90c9441012d4b3ffc21bbfb&id=maa443089855";
-      const url = "https://jsonplaceholder.typicode.com/todos";
+      const actual = "https://inrdeals.com/fetch/stores?token=2c3f5a1662f83d1db90c9441012d4b3ffc21bbfb&id=maa443089855";
+      // const url = "https://jsonplaceholder.typicode.com/todos";
 
       try {
         // Make a GET request to a URL
-        const response = await axios.get(url);
+        const response = await axios.get(actual);
         setData(response.data);
         setLoading(false);
-        console.log(response.data);
       } catch (error) {
         // Handle any errors
         setError(error);
         setLoading(false);
         console.error('Error:', error);
       }
+      
     };
-
+    
     fetchData();
   }, []);
-
+   console.log(data);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "https://inrdeals.com/fetch/stores":
-        "http://localhost:5173/",
-    },
-  },
 });
