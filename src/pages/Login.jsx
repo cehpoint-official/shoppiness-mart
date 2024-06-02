5cb357a4212a5235c603ce7d2d6bed277ba3fb3d
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-06-02 09:52:25 +0530

    responsive login and signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,26 +21,31 @@ const Login = () => {
 
   return (
     <>
-      <div className="overflow-hidden ">
-        <div className="grid grid-cols-1 sm:grid-cols-2 ">
-          <div className="">
-            <div className="absolute  ">
-              <img src={Backimg} alt="" className=" " />
+      <div className="overflow-hidden">
+        <div className="flex flex-wrap">
+          {/* <!-- Left Column --> */}
+          <div className="w-full sm:w-1/2  relative">
+            <div className="absolute inset-0">
+              <img
+                src={Backimg}
+                alt="Background"
+                className="w-full h-full object-cover"
+              />
             </div>
-            <div className="relative ">
+            <div className="relative z-10">
               <div className="mx-10 mt-5">
-                <img src={ShoppingBag2} alt="" className="h-11   " />
+                <img src={ShoppingBag2} alt="Shopping Bag" className="h-11" />
               </div>
-              <div className="">
-                <img src={Loginimg} alt="" className="" />
+              <div>
+                <img src={Loginimg} alt="Login" />
               </div>
             </div>
           </div>
 
-          {/* Right column */}
-          <div className="">
-            <div className="">
-              <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
+          {/* <!-- Right Column --> */}
+          <div className="w-full sm:w-1/2 flex flex-col justify-center p-4">
+            <div className="w-full max-w-md mx-auto">
+              <div className="flex flex-col gap-4">
                 <p className="text-4xl font-semibold text-center mt-10 md:mt-12">
                   Login
                 </p>
@@ -48,11 +53,8 @@ const Login = () => {
                   Welcome! Login in to your account
                 </p>
 
-                <div className="mx-auto md:mx-32 mt-3">
-                  <label
-                    htmlFor="email"
-                    className="block mb-1 text-gray-600 mx-24   "
-                  >
+                <div className="mt-3">
+                  <label htmlFor="email" className="block mb-1 text-gray-600">
                     Email Address
                   </label>
                   <input
@@ -61,13 +63,13 @@ const Login = () => {
                       setUserData({ ...userData, email: e.target.value })
                     }
                     value={userData.email}
-                    className="w-64 md:w-80 p-2 border m border-gray-200 bg-slate-100 rounded mx-24"
+                    className="w-full p-2 border border-gray-200 bg-slate-100 rounded"
                   />
                 </div>
-                <div className="mx-auto md:mx-32">
+                <div>
                   <label
                     htmlFor="password"
-                    className="block mb-1 text-gray-600 mx-24"
+                    className="block mb-1 text-gray-600"
                   >
                     Password
                   </label>
@@ -77,47 +79,42 @@ const Login = () => {
                     }
                     type="password"
                     value={userData.password}
-                    className="w-64 md:w-80 p-2 border border-gray-200 bg-slate-100 rounded mx-24"
+                    className="w-full p-2 border border-gray-200 bg-slate-100 rounded"
                   />
                 </div>
-                <a href="" className="text-blue-600 underline text-center ">
-                  I’ve forgotten password
+                <a href="#" className="text-blue-600 underline text-center">
+                  I ve forgotten password
                 </a>
 
                 <Link
                   to="/"
-                  onClick={(e) => login(userData)}
-                  className="bg-[#049D8E] text-white mx-28 text-center  md:mx-56 w-64 md:w-80 px-4 py-2 rounded mt-14 md:mt-6"
+                  onClick={() => login(userData)}
+                  className="bg-[#049D8E] text-white text-center w-full py-2 rounded mt-6"
                 >
                   Login
                 </Link>
                 <div className="text-gray-500 text-center mt-5">or</div>
               </div>
 
-              <div className="mt-5 md:flex md:justify-center">
-                <div className="mx-44 md:mx-0 md:mr-5 ">
-                  <img
-                    src={Googleicon}
-                    alt=""
-                    onClick={gooleLogin}
-                    className="border-2 px-8 py-2 rounded-md  md:w-1/2 lg:w-auto"
-                  />
-                </div>
-
-                <div className="mx-44 md:mx-0 mt-5 md:mt-0 ">
-                  <img
-                    src={Facebookicon}
-                    alt=""
-                    className="border-2 px-8 py-2 rounded-md md:w-1/2 lg:w-auto"
-                  />
-                </div>
+              <div className="mt-5 flex justify-center gap-5">
+                <img
+                  src={Googleicon}
+                  alt="Google Login"
+                  onClick={gooleLogin}
+                  className="border-2 px-8 py-2 rounded-md cursor-pointer"
+                />
+                <img
+                  src={Facebookicon}
+                  alt="Facebook Login"
+                  className="border-2 px-8 py-2 rounded-md cursor-pointer"
+                />
               </div>
               <div className="mt-7 text-center">
                 <p className="font-medium text-lg">
                   Don’t have an account?{" "}
                   <a
                     href="/signup"
-                    className=" text-[#049D8E] font-medium underline"
+                    className="text-[#049D8E] font-medium underline"
                   >
                     Signup
                   </a>
